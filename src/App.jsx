4cbc60619dd033032eecdc31725435e46e64ead7
michaelluoyunhan <michael_luoyunhan@outlook.com>
2024-10-09 00:05:34 -0400

    try

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,112 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios';
+import React, { Component } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Spinner from 'react-bootstrap/Spinner';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+class App extends Component {
+  state = {
+    isLoadingVisible: false,
+    val: '',
+    imgSrc: '',
+  };
+
+  showLoading = () => {
+    this.setState({ isLoadingVisible: true });
+  };
+
+  hideLoading = () => {
+    this.setState({ isLoadingVisible: false });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.showLoading();
+
+    console.log(prompt);
+    console.log(process.env.NODE_ENV);
+
+    const api =
+      process.env.NODE_ENV === 'development'
+        ? '/test/stabled'
+        : 'https://unuqz0uxn9.execute-api.us-east-1.amazonaws.com/test/stabled';
+
+    const data = { data: e.target.searchQuery.value };
+    console.log(data);
+    axios({
+      method: 'POST',
+      data: JSON.stringify(data),
+      headers: { 'Content-Type': 'application/json' },
+      url: api,
+    })
+      .then((response) => {
+        console.log(response);
+        this.setState({ imgSrc: response.data.body });
+
+        setTimeout(() => {
+          this.hideLoading();
+          this.setState({ val: '' });
+        }, 500);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  render() {
+    return (
+      <Container className='p-5' id='container' name='container'>
+        <h1> Welcome to Stable Diffusion AI</h1>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group className='mb-3' controlId='formBasicEmail'>
+            <Form.Label>Enter Text to convert Image</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Enter text to convert image'
+              required
+              autoFocus={true}
+              name='searchQuery'
+              controlId='searchQuery'
+              defaultValue={this.state.val}
+            />
+            <Form.Text className='text-muted'>
+              We'll attempt our best to create image you will love it!
+            </Form.Text>
+          </Form.Group>
+          <Button
+            variant='primary'
+            type='submit'
+            className='btn btn-primary btn-large centerButton'
+          >
+            Submit
+          </Button>
+
+          <Image
+            id='myImage'
+            className='img-fluid shadow-4'
+            src={this.state.imgSrc}
+          />
+        </Form>
+        {this.state.isLoadingVisible && (
+          <div id='backdrop'>
+            <Button variant='primary' disabled>
+              <Spinner
+                target='container'
+                as='span'
+                animation='grow'
+                size='sm'
+                role='status'
+                aria-hidden='true'
+              />
+              Loading...
+            </Button>
+          </div>
+        )}
+      </Container>
+    );
+  }
 }
 
-export default App
+export default App;
